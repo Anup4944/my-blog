766a275670aa548e4bfcc323b4dc5dd1c51bd1b4
Anup <anuppoudel60@gmail.com>
2022-06-21 19:23:11 +1000

    final commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Anup's Blog
+              BLOG
             </span>
           </Link>
         </div>
